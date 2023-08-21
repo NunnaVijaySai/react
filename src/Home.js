2ffd2de1b33ead59e369e67c7212237240687f9c
NunnaVijaySai <vijaysai.nunna5104@gmail.com>
2023-08-21 18:53:35 +0530

    first commit

---
+++ src/Home.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+export default class Home extends Component {
+  state={
+    username:"",
+    password:""
+  }
+  handleChange=(e)=>{
+    this.setState({
+      [e.target.name]:e.target.value
+    })
+  }
+  handleLogin=()=>{
+    axios.post("https://backendjap.onrender.com/add",
+    {
+      username:this.state.username,
+      password:this.state.password
+    })
+    .then((res)=>{
+      console.log(res)
+      alert(res.data)
+    })
+  }
+  render() {
+    return (
+      <div>
+        <p><input name='username' placeholder='Username' onChange={this.handleChange}/>
+       </p>
+       <p><input name='password' placeholder='Password' onChange={this.handleChange}/>
+       </p>
+        <button onClick={this.handleLogin}>Login</button>
+      </div>
+    )
+  }
+}
